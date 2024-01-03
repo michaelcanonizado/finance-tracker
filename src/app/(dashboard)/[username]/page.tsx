e8523b71d6afc7d8658a1880452bae03b09dfbe9
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-04 00:03:31 +0800

    test: made initial google sheets append function

--- src/app/(dashboard)/[username]/page.tsx
+++ src/app/(dashboard)/[username]/page.tsx
@@ -1,6 +1,10 @@
+import { sendCashFlowData } from "@/lib/sendCashFlowData";
+import { GoogleSheets } from "@/types/main";
 import React from "react";
 
 const Home = async () => {
+  const res = await sendCashFlowData(GoogleSheets.test);
+
   return (
     <div className="min-h-screen p-6">
       <h1>Dashboard</h1>
