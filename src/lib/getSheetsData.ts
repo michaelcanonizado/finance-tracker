c38018bcb29f9361b83a96637dc8eda48fa5e508
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-03 22:47:24 +0800

    refactor: separated formatDate function into a separate files in helpers

--- src/lib/getSheetsData.ts
+++ src/lib/getSheetsData.ts
@@ -1,6 +1,7 @@
 import { GoogleSheets, ICashFlow } from "@/types/main";
 
 import { authenticateGoogleSheets } from "./authenticateGoogleSheets";
+import { formatDate } from "@/helpers/formatDate";
 
 export const getData = async (sheetName: GoogleSheets): Promise<ICashFlow> => {
   // Authenticate google sheets and get instances
@@ -21,26 +22,6 @@ export const getData = async (sheetName: GoogleSheets): Promise<ICashFlow> => {
     values: formattedData.values,
     total: formattedData.total,
   } as ICashFlow;
-};
-
-const formatDate = (date: string): string => {
-  // Formats date from 1/2/2024 to Tues, 1/2/2024
-  // options is type asserted as it is throwing an error. More infomartion on: https://stackoverflow.com/questions/73563950/what-exactly-is-the-typescript-linter-asking-for-in-this-case-where-an-object-r
-  const dateOptions = {
-    weekday: "short" as const,
-    year: "numeric" as const,
-    month: "numeric" as const,
-    day: "numeric" as const,
-  };
-
-  const dateObj = new Date(date);
-
-  // Shortens the year further from 2024 to 24 (final date format: 'Tues, 1/2,24')
-  let shortYear = dateObj.getFullYear().toString().slice(-2);
-
-  return dateObj
-    .toLocaleString("en-US", dateOptions)
-    .replace(/\d{4}/, shortYear);
 };
 
 const formatGoogleSheetData = (
