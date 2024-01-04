67318bb1516c422828fb07008053d498c3118ddf
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-04 11:34:26 +0800

    refactor: removed sendCashFlowData function call, that was used in previous test, to prevent appends to spreadsheet

--- src/app/(dashboard)/[username]/page.tsx
+++ src/app/(dashboard)/[username]/page.tsx
@@ -1,10 +1,6 @@
-import { sendCashFlowData } from "@/lib/sendCashFlowData";
-import { GoogleSheets } from "@/types/main";
 import React from "react";
 
 const Home = async () => {
-  const res = await sendCashFlowData(GoogleSheets.test);
-
   return (
     <div className="min-h-screen p-6">
       <h1>Dashboard</h1>
