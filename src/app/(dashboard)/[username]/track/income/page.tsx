409a78bbb8baa8d864f14ec42989f40fe5c89b72
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2024-01-05 12:55:02 +0800

    refactor: rename RecordIncome and RecordExpense to TrackIncome and TrackExpense

--- src/app/(dashboard)/[username]/track/income/page.tsx
+++ src/app/(dashboard)/[username]/track/income/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import DashboardRecordForm from "@/components/dashboard/track/DashboardRecordForm";
 
-const RecordIncome = () => {
+const TrackIncome = () => {
   return (
     <div className="mx-auto w-fit">
       <div className="mt-8 rounded border px-6 py-14 sm:px-12">
@@ -15,4 +15,4 @@ const RecordIncome = () => {
   );
 };
 
-export default RecordIncome;
+export default TrackIncome;
